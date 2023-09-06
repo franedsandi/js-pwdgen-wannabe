0d476bb92cf3927e68820f48b20e3f148a9a5486
franedsandi <franedsandi@gmail.com>
2023-09-06 15:55:33 +0200

    [feat] (js) step by step + soluzione

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Fare un  (insicurissimo) password generator in base al nome, cognome e colore preferito + 23 
+
+//1. Salvare in 4 variabili nel prompt dell'inserimento del nome, cognome, colore preferito e il numero
+
+//2. Creare la stringa di output
+
+//3. effettuare l'innerHTML dell'output nel tag designato
+
+// 1.
+const name = prompt('Inserire il nome');
+const lastname = prompt('Inserire il cognome');
+const colore = prompt('Inserire il colore preferito');
+const number = '23';
+
+//2.
+const message = 'Ciao ' + name + ' ' + lastname + ' la tua (insicurissima) password e: <br>"'+ name + lastname + colore + number + '" ' ;
+
+//3. 
+document.getElementById('output').innerHTML = message;
+console.log(message);
